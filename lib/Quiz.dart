fddcf5b52d275657454b0e97d45f9d1fefbc4dbd
Arun Prasad <aarunprasad92@gmail.com>
2024-06-19 00:44:33 +0530

    Render content conditionally using state

---
+++ lib/Quiz.dart
@@ -0,0 +1,40 @@
+import 'package:first_app/questions_screen.dart';
+import 'package:first_app/start_screen.dart';
+import 'package:flutter/material.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+            body: Container(
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(colors: [
+                  Color.fromARGB(255, 51, 124, 53),
+                  Color.fromARGB(255, 239, 244, 239)
+                ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+                child: activeScreen)));
+  }
+}
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,10 +1,9 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +27,7 @@ class StartScreen extends StatelessWidget {
           height: 30,
         ),
         OutlinedButton.icon(
-          onPressed: () {},
+          onPressed: startQuiz,
           style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
           icon: const Icon(Icons.arrow_right_alt),
           label: const Text('Start Quiz'),
