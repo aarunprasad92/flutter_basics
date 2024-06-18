66949a2bf52a6a725dabc3b358d0c2cb195fa1e8
Arun Prasad <aarunprasad92@gmail.com>
2024-06-19 00:03:17 +0530

    Add stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,6 @@
-import 'package:first_app/start_screen.dart';
+import 'package:first_app/Quiz.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-      home: Scaffold(
-          body: Container(
-              decoration: const BoxDecoration(
-                  gradient: LinearGradient(colors: [
-                Color.fromARGB(255, 51, 124, 53),
-                Color.fromARGB(255, 239, 244, 239)
-              ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-              child: const StartScreen()))));
+  runApp(const Quiz());
 }
