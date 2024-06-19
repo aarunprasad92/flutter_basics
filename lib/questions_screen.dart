b143ad6f4b33bdf87e5f240d20d3e0afc4f86d51
Arun Prasad <aarunprasad92@gmail.com>
2024-06-19 12:15:54 +0530

    Align the widgets in centre using sized box and column axis alignment

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -11,17 +11,21 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        const Text('Question 1'),
-        const SizedBox(
-          height: 30,
-        ),
-        ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
-        ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
-        ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
-        ElevatedButton(onPressed: () {}, child: const Text('Answer 4'))
-      ],
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Question 1'),
+          const SizedBox(
+            height: 30,
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 1')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 2')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 3')),
+          ElevatedButton(onPressed: () {}, child: const Text('Answer 4'))
+        ],
+      ),
     );
   }
 }
