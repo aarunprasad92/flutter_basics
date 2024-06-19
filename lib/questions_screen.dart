08c8558336e5bed1cd48e81eafd175fb04537a49
Arun Prasad <aarunprasad92@gmail.com>
2024-06-19 21:43:30 +0530

    Use google fonts for text styling

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,6 +1,7 @@
 import 'package:first_app/answer_button.dart';
 import 'package:first_app/data/questions.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -32,7 +33,10 @@ class _QuestionsScreenState extends State<QuestionsScreen> {
           children: [
             Text(
               currentQuestion.question,
-              style: const TextStyle(color: Colors.white, fontSize: 20),
+              style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
               textAlign: TextAlign.center,
             ),
             const SizedBox(
