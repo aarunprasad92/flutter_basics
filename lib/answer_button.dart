5378cc8edf33e17b72a1a157baec1a36bc42b53e
Arun Prasad <aarunprasad92@gmail.com>
2024-06-19 21:28:32 +0530

    Align text to center

--- lib/answer_button.dart
+++ lib/answer_button.dart
@@ -25,7 +25,10 @@ class AnswerButton extends StatelessWidget {
           borderRadius: BorderRadius.circular(8),
         ),
       ),
-      child: Text(answerText),
+      child: Text(
+        answerText,
+        textAlign: TextAlign.center,
+      ),
     );
   }
 }
