3712678bb410fe8805510f562eedcd4798b2ffec
Arun Prasad <aarunprasad92@gmail.com>
2024-06-19 11:36:52 +0530

    Render conditionally without using initstate

--- lib/Quiz.dart
+++ lib/Quiz.dart
@@ -11,17 +11,11 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  Widget? activeScreen;
-
-  @override
-  void initState() {
-    activeScreen = StartScreen(switchScreen);
-    super.initState();
-  }
+  var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
-      activeScreen = const QuestionsScreen();
+      activeScreen = 'questions-screen';
     });
   }
 
@@ -35,6 +29,8 @@ class _QuizState extends State<Quiz> {
                   Color.fromARGB(255, 51, 124, 53),
                   Color.fromARGB(255, 239, 244, 239)
                 ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
-                child: activeScreen)));
+                child: activeScreen == 'start-screen'
+                    ? StartScreen(switchScreen)
+                    : const QuestionsScreen())));
   }
 }
