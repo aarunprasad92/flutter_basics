a98a49a481c69edf8a3c38f443763e1af008f0f2
Arun Prasad <aarunprasad92@gmail.com>
2024-06-21 12:26:30 +0530

    Add space between rows built using list by wrapping inside a column

--- lib/questions_summary.dart
+++ lib/questions_summary.dart
@@ -11,31 +11,38 @@ class QuestionsSummary extends StatelessWidget {
       child: SingleChildScrollView(
         child: Column(
           children: summaryData.map((data) {
-            return Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              Container(
-                  width: 35,
-                  height: 35,
-                  decoration: BoxDecoration(
-                    color: data['correct_answer'] == data['user_answer']
-                        ? const Color.fromARGB(255, 5, 81, 8)
-                        : const Color.fromARGB(255, 205, 128, 122),
-                    shape: BoxShape.circle,
-                  ),
-                  alignment: Alignment.center,
-                  child: Text(
-                    ((data['question_index'] as int) + 1).toString(),
-                    style: const TextStyle(color: Colors.white),
-                  )),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text(data['question'] as String),
-                    Text(data['correct_answer'] as String),
-                    Text(data['user_answer'] as String)
-                  ],
-                ),
-              )
-            ]);
+            return Column(
+              children: [
+                Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                  Container(
+                      width: 35,
+                      height: 35,
+                      decoration: BoxDecoration(
+                        color: data['correct_answer'] == data['user_answer']
+                            ? const Color.fromARGB(255, 5, 81, 8)
+                            : const Color.fromARGB(255, 205, 128, 122),
+                        shape: BoxShape.circle,
+                      ),
+                      alignment: Alignment.center,
+                      child: Text(
+                        ((data['question_index'] as int) + 1).toString(),
+                        style: const TextStyle(color: Colors.white),
+                      )),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(data['question'] as String),
+                        Text(data['correct_answer'] as String),
+                        Text(data['user_answer'] as String)
+                      ],
+                    ),
+                  )
+                ]),
+                const SizedBox(
+                  height: 30,
+                )
+              ],
+            );
           }).toList(),
         ),
       ),
